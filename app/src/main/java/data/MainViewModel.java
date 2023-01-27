1bab470b74dffd7975d117cb5b9f464d7becd7fd
Rajvir0900 <kaurrajvir2003@gmail.com>
2023-01-27 13:05:13 -0500

    seventh commit: adding compound button

--- app/src/main/java/data/MainViewModel.java
+++ app/src/main/java/data/MainViewModel.java
@@ -1,16 +1,12 @@
 package data;
 
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
+
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
     public MutableLiveData<String> editString = new MutableLiveData<>();
-      model.editString.qbserve(owner:this, new Observer<String>(){
-        @Override
-        public void onChanged(String s){
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
 
-        }
-      });
 }
--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.cst2335.Kaur0900.R;
 import com.cst2335.Kaur0900.databinding.ActivityMainBinding;
@@ -19,25 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        model = new ViewModelProvider(owner:this).get(MainViewModel.class);
 
+        model = new ViewModelProvider(this).get(MainViewModel.class);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-        variableBinding.mytext.setText(model.editString);
+        //variableBinding.textview.setText(model.editString);
         variableBinding.mybutton.setOnClickListener(click ->
         {
-            model.editString = variableBinding.myEditText.getText().toString();
-            variableBinding.mytext.setText("Your edit text has:"+model.editString);
+            model.editString.postValue(variableBinding.myedittext.getText().toString());
+            //variableBinding.textview.setText("Your edit text has:"+model.editString);
         });
         model.editString.observe(this, s -> {
-            variableBinding.metext.setText("Your edit text has"+ s );
+            variableBinding.textview.setText("Your edit text has"+ s );
         });
-        model.isSelected.observe( this , selected -> {
-            variableBinding.checkBox.setChecked(selected);
-            variableBinding.radioButton.setChecked(selected);
-            variableBinding.switch1.setChecked(selected);
-        })
+
 
 
 
